94f86657a5e1bc7202e96ec3e2882f6ed3da52d1
Justin Hirata <jlhirata88@aol.com>
2018-12-19 19:10:17 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -16,6 +16,7 @@ function inc(n){
 function dec(n){
   return n-=1
 }
+<<<<<<< HEAD
 function makeInt(n) {
   return parseInt(n,10)
 }
@@ -24,6 +25,27 @@ function preserveDecimal(n){
 }
 
 
+=======
+function makeInt(n){
+  return parseInt('100',10)
+}
+function makeInt(n){
+  return parseInt('765',0)
+}
+function makeInt(n){
+  return parseInt("nonsense", "10")
+}
+function preserveDecimal(n){
+  return parseFloat(282)
+}
+function preserveDecimal(n){
+  return parseFloat("2.222")
+}
+
+
+
+
+>>>>>>> 3a2a2ecf94ab325d713c488ecf061bb6895dfdc3
 add()
 subtract()
 muliply()
@@ -32,4 +54,7 @@ inc()
 dec()
 makeInt()
 preserveDecimal()
+<<<<<<< HEAD
 
+=======
+>>>>>>> 3a2a2ecf94ab325d713c488ecf061bb6895dfdc3
